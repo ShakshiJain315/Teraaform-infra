b3831573522cf17290b19e4193ef73fed524aa2b
shakshi <jshakshi301@gmail.com>
2023-03-30 15:24:13 -0400

    modified the files

--- s3.tf
+++ s3.tf
@@ -1,10 +1,11 @@
-resource "aws_s3_bucket" "my-s3-bucket9" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  
-   versioning {
-    enabled = var.versioning
-  }
-  
-  tags = var.tags
-}
+//resource "aws_s3_bucket" "my-s3-bucket9" {
+//bucket_prefix = var.bucket_prefix
+//acl           = var.acl
+
+
+//versioning {
+//enabled = var.versioning
+//}
+
+//tags = var.tags
+//}
