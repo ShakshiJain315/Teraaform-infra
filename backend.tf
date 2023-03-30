c51b7a78726dc63a6029855dc40c5a43a09bd48d
ShakshiJain315 <117496183+ShakshiJain315@users.noreply.github.com>
2023-03-30 12:13:02 -0400

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "my-aws-tf-state-bucket5"
+    bucket = "my-aws-tf-state-bucket9"
     key = "main"
-    region = "us-east-1"
+    region = "us-east-2"
     dynamodb_table = "my-dynamo-db-table"
   }
 }
